0113e0f81360312836187ede77e79730933f453d
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2024-02-12 02:20:17 +0200

    Adding First Function

--- MCAL/RCC_Driver/RCC.c
+++ MCAL/RCC_Driver/RCC.c
@@ -1,6 +1,8 @@
-/***************************************/
-/*			Masks			  		   */
-/***************************************/
+#include "RCC.h"
+
+/********************************************/
+/*			Masks				  	   		*/
+/********************************************/
 #define MASK_HSIRDY					0x00000002
 #define MASK_HSERDY					0x00020000
 #define MASK_PLLRDY					0x02000000
@@ -16,3 +18,34 @@
 #define SHIFT_PLL_Q					22
 
 #define MASK_GET_SYSCLK				0x0000000C
+
+/********************************************/
+/*			Function Implementations 		*/
+/********************************************/
+RCC_ErrorStatus_t RCC_ControlClock (u8 ClockStatus, u8 Clock)
+{
+	RCC_ErrorStatus_t RET_ErrorStatus=RCC_OK;
+
+	if(!(Clock==CLOCK_HSI||Clock==CLOCK_HSE||Clock==CLOCK_PLL))
+	{
+		RET_ErrorStatus=RCC_InvalidClock;
+	}
+	else if(!(ClockStatus==CLOCK_STATUS_OFF||ClockStatus==CLOCK_STATUS_ON))
+	{
+		RET_ErrorStatus=RCC_InvalidClockStatus;
+	}
+	else
+	{
+		RET_ErrorStatus=RCC_OK;
+		switch(ClockStatus)
+		{
+		case CLOCK_STATUS_ON:
+			RCC_CR|=Clock;
+		break;
+		case CLOCK_STATUS_OFF:
+			RCC_CR&=~Clock;
+		break;
+		}
+	}
+	return RET_ErrorStatus;
+}
