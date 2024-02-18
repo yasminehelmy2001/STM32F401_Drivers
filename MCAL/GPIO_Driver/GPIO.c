60e686920c6eafcf3cad5ef782046dbedf2e45b4
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2024-02-18 04:51:12 +0200

    GPIO Driver ~Semi Tested~

---
+++ MCAL/GPIO_Driver/GPIO.c
@@ -0,0 +1,180 @@
+#include "GPIO.h"
+
+#define ONE_BIT_MASK					0x00000001
+#define TWO_BIT_MASK					0x00000003
+#define FOUR_BIT_MASK					0x0000000F
+
+#define MODE_MASK						0x03
+#define OUTPUT_TYPE_MASK				0x10
+#define PUPD_MASK						0x0C
+
+#define SHIFT_2							(2U)
+#define SHIFT_4							(4U)
+
+#define INPUT_MODE						(0U)
+#define OUTPUT_MODE						(1U)
+#define AF_MODE							(2U)
+#define ANALOG_MODE						(3U)
+
+#define PUPD_DEACTIVATED				(0U)
+#define PUPD_PULL_UP					(1U)
+#define PUPD_PULL_DOWN					(2U)
+
+#define OUTPUT_TYPE_PP					(0U)
+#define OUTPUT_TYPE_OD					(1U)
+#define OUTPUT_TYPE_DEACTIVATED			(2U)
+
+/**
+ * @brief	Function that Initializes a GPIO PIN
+ *
+ * @param	Pointer to GPIO_Pin_t Struct, contains:
+ *
+ * 			- Port 			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ *
+ * 			- Pin  			(PIN0->PIN15)
+ *
+ * 			- Speed			(Macro begins with SPEED_)
+ *
+ * 			- Mode			# (Macro begins with MODE_)
+ * 							# Selects mode from Input/Output/Alternating Function/Analog
+ * 							# If Mode is Output -> Select whether Output Type is PP(Push Pull) or OD(Open Drain)
+ * 							# Select if circuit is PU(Pull Up), PD(Pull Down), or Deactivated
+ *
+ *
+ * 			- AF_CHOICE		# Selects Alternating Function from 16 choices
+ * 							# (Macro begins with AF)
+ * 							# Choices: AF0->AF_15 or AF_DEACTIVATED
+ */
+GPIO_ErrorStatus_t GPIO_InitPin(GPIO_Pin_t*PinCfg)
+{
+	GPIO_ErrorStatus_t RET_ErrorStatus=GPIO_Ok;
+
+	GPIO_Registers_t volatile *Port= (GPIO_Registers_t volatile *) (PinCfg->Port);
+
+	u32 Pin= PinCfg->Pin;
+	u32 Speed= PinCfg->Speed;
+	u32 Mode= (PinCfg->Mode&MODE_MASK);
+	u32 OutputType=(PinCfg->Mode&OUTPUT_TYPE_MASK)>>SHIFT_4;
+	u32 PUPD= (PinCfg->Mode&PUPD_MASK)>>SHIFT_2;
+	u32 AF_Choice= PinCfg->AF_Choice;
+
+	if(!(((Mode>=INPUT_MODE)&&(Mode<=ANALOG_MODE))&&
+	  ((OutputType>=OUTPUT_TYPE_PP)&&(OutputType<=OUTPUT_TYPE_DEACTIVATED)) &&
+	  ((PUPD==PUPD_PULL_UP)||(PUPD==PUPD_PULL_DOWN)||(PUPD==PUPD_DEACTIVATED)) &&
+	  ((Port==GPIOA)||(Port==GPIOB)||(Port==GPIOC)||(Port==GPIOD)||(Port==GPIOE)||(Port==GPIOH)) &&
+	  (Pin>=PIN0&&Pin<=PIN15)))
+
+	  {
+		RET_ErrorStatus=GPIO_InvalidParameter;
+	  }
+
+	else
+	{
+
+		u32 Loc_OSPEEDR= Port->OSPEEDR;
+		Loc_OSPEEDR&=~(TWO_BIT_MASK<<Pin*SHIFT_2);
+		Loc_OSPEEDR|=(Speed<<Pin*SHIFT_2);
+		Port->OSPEEDR= Loc_OSPEEDR;
+
+		u32 Loc_MODER= Port->MODER;
+		Loc_MODER&=~(TWO_BIT_MASK<<Pin*SHIFT_2);
+		Loc_MODER|=(Mode<<Pin*SHIFT_2);
+		Port->MODER= Loc_MODER;
+
+		u32 Loc_PUPDR= Port->PUPDR;
+		Loc_PUPDR&=~(TWO_BIT_MASK<<Pin*SHIFT_2);
+		Loc_PUPDR|=(PUPD<<Pin*SHIFT_2);
+		Port->PUPDR=Loc_PUPDR;
+
+		if(!(OutputType==OUTPUT_TYPE_DEACTIVATED))
+		{
+			u32 Loc_OTYPER=Port->OTYPER;
+			Loc_OTYPER&=~(ONE_BIT_MASK<<Pin);
+			Loc_OTYPER|=(OutputType<<Pin);
+			Port->OTYPER=Loc_OTYPER;
+		}
+		else
+		{
+			//Do Nothing
+		}
+
+		if(!(AF_Choice==AF_DEACTIVATED))
+		{
+			if(Pin<PIN8)
+			{
+				u32 Loc_AFRL= Port->AFRL;
+				Loc_AFRL&=~(FOUR_BIT_MASK<<Pin*SHIFT_4);
+				Loc_AFRL|=(AF_Choice<<Pin*SHIFT_4);
+				Port->AFRL=Loc_AFRL;
+			}
+			else
+			{
+				u32 Loc_AFRH= Port->AFRH;
+				Loc_AFRH&=~(FOUR_BIT_MASK<<Pin*SHIFT_4);
+				Loc_AFRH|=(AF_Choice<<Pin*SHIFT_4);
+				Port->AFRL=Loc_AFRH;
+			}
+		}
+		else
+		{
+			//Do Nothing
+		}
+	}
+
+	return RET_ErrorStatus;
+}
+
+/**
+ * @brief	Function that Sets a GPIO Pin to either HIGH/LOW
+ *
+ * @param	-Port			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ * 			-Pin			(PIN0->PIN15)
+ * 			-Value			(GPIO_LOW/GPIO_HIGH)
+ *
+ * @error	Error Status    GPIO_InvalidParameter/ GPIO_Ok
+ */
+GPIO_ErrorStatus_t GPIO_SetPinValue(void*Port, u8 Pin,u8 Value)
+{
+	GPIO_ErrorStatus_t RET_ErrorStatus=GPIO_Ok;
+    GPIO_Registers_t* Loc_Port = (GPIO_Registers_t*)Port;
+    if(!(((Value==GPIO_HIGH)||(Value==GPIO_LOW))&&
+      ((Port==GPIOA)||(Port==GPIOB)||(Port==GPIOC)
+      ||(Port==GPIOD)||(Port==GPIOE)||(Port==GPIOH))))
+	{
+		RET_ErrorStatus=GPIO_InvalidParameter;
+	}
+    else if(Value==GPIO_LOW)
+    {
+    	Loc_Port->BSRR = 1<<(Pin+16);
+    }
+    else
+    {
+    	Loc_Port->BSRR = 1<<Pin;
+    }
+	return RET_ErrorStatus;
+}
+
+/**
+ * @brief	Function that Gets a GPIO Pin Value
+ *
+ * @param	-Port			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ * 			-Pin			(PIN0->PIN15)
+ * 			-*Value			(GPIO_LOW/GPIO_HIGH)
+ *
+ * @error	Error Status    GPIO_InvalidParameter/ GPIO_Ok
+ */
+GPIO_ErrorStatus_t GPIO_GetPinValue(void*Port, u8 Pin,u8*Value)
+{
+	GPIO_ErrorStatus_t RET_ErrorStatus=GPIO_Ok;
+    GPIO_Registers_t* Loc_Port = (GPIO_Registers_t*)Port;
+    if(Loc_Port->ODR&(1<<Pin))
+	{
+		*Value=GPIO_HIGH;
+	}
+	else
+	{
+		*Value=GPIO_LOW;
+	}
+	return RET_ErrorStatus;
+}
+
---
+++ MCAL/GPIO_Driver/GPIO.h
@@ -0,0 +1,196 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "std_types.h"
+
+/**************************************************************************/
+/*						PORT MACROS				 					 	  */
+/**************************************************************************/
+#define GPIOA						(void*)(0x40020000)
+#define GPIOB						(void*)(0x40020400)
+#define GPIOC						(void*)(0x40020800)
+#define GPIOD						(void*)(0x40020C00)
+#define GPIOE						(void*)(0x40021000)
+#define GPIOH						(void*)(0x40021C00)
+
+/**************************************************************************/
+/*						PIN MACROS				 					 	  */
+/**************************************************************************/
+#define PIN0						0
+#define PIN1						1
+#define PIN2						2
+#define PIN3						3
+#define PIN4						4
+#define PIN5						5
+#define PIN6						6
+#define PIN7						7
+#define PIN8						8
+#define PIN9						9
+#define PIN10						10
+#define PIN11						11
+#define PIN12						12
+#define PIN13						13
+#define PIN14						14
+#define PIN15						15
+
+/**************************************************************************/
+/*						ALTERNATING FUNCTION OPTION  MACROS	 		 	  */
+/**************************************************************************/
+#define AF0							(0U)
+#define AF1							(1U)
+#define AF2							(2U)
+#define AF3							(3U)
+#define AF4							(4U)
+#define AF5							(5U)
+#define AF6							(6U)
+#define AF7							(7U)
+#define AF8							(8U)
+#define AF9							(9U)
+#define AF10						(10U)
+#define AF11						(11U)
+#define AF12						(12U)
+#define AF13						(13U)
+#define AF14						(14U)
+#define AF15						(15U)
+#define AF_DEACTIVATED				(16U)
+
+/**************************************************************************/
+/*						MODES MACROS								 	  */
+/**************************************************************************/
+#define MODE_OUTPUT_PP				0x01
+#define MODE_OUTPUT_PP_PU			0x05
+#define MODE_OUTPUT_PP_PD			0x09
+#define MODE_OUTPUT_OD				0x11
+#define MODE_OUTPUT_OD_PU			0x15
+#define MODE_OUTPUT_OD_PD			0x19
+#define MODE_AF_PP					0x08
+#define MODE_AF_PP_PU				0x02
+#define MODE_AF_PP_PD				0x06
+#define MODE_AF_OD					0x0A
+#define MODE_AF_OD_PU				0x12
+#define MODE_AF_OD_PD				0x16
+#define MODE_INPUT_FLOATING			0x1A
+#define MODE_INPUT_PU				0x04
+#define MODE_INPUT_PD				0x08
+#define MODE_ANALOG					0x03
+
+/**************************************************************************/
+/*						SPEED MACROS			 					 	  */
+/**************************************************************************/
+#define SPEED_LOW					(0U)
+#define SPEED_MEDIUM				(1U)
+#define SPEED_HIGH					(2U)
+#define SPEED_VERY_HIGH				(3U)
+
+/**************************************************************************/
+/*						GPIO LOW/HIGH MACRO		 					 	  */
+/**************************************************************************/
+#define GPIO_LOW					(0U)
+#define GPIO_HIGH					(1U)
+
+/**************************************************************************/
+/*						GPIO Registers Struct			 			 	  */
+/**************************************************************************/
+typedef struct
+{
+	volatile u32 MODER;
+	volatile u32 OTYPER;
+	volatile u32 OSPEEDR;
+	volatile u32 PUPDR;
+	volatile u32 IDR;
+	volatile u32 ODR;
+	volatile u32 BSRR;
+	volatile u32 LCKR;
+	volatile u32 AFRL;
+	volatile u32 AFRH;
+} GPIO_Registers_t;
+
+
+/**************************************************************************/
+/*						GPIO Pin Configuration Struct	 			 	  */
+/**************************************************************************/
+typedef struct
+{
+	void* Port; 		 /* GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH */
+	u8 Pin;  			 /* PIN0 -> PIN15 */
+
+	u8 Speed;			 /* SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH, SPEED_VERY_HIGH */
+
+	u8 Mode;   		 	 /* # MACRO Starts with MODE_
+  							# Selects mode from Input/Output/Alternating Function/Analog
+ 							# If Mode is Output -> Select whether Output Type is PP(Push Pull) or OD(Open Drain)
+  							# Select if circuit is PU(Pull Up), PD(Pull Down), or Deactivated */
+
+
+	u8 AF_Choice; 		 /* # Macro begins with AF
+							# Selects Alternating Function from 16 choices
+							# Choices: AF0->AF_15 or AF_DEACTIVATED
+							# You should enable your selected peripheral from RCC! */
+
+
+} GPIO_Pin_t;
+
+/**************************************************************************/
+/*						GPIO ERROR STATUS		 					 	  */
+/**************************************************************************/
+typedef enum
+{
+	GPIO_Ok,
+	GPIO_Nok,
+	GPIO_InvalidParameter
+}GPIO_ErrorStatus_t;
+
+/**************************************************************************/
+/*						FUNCTION PROTOTYPES		 					 	  */
+/**************************************************************************/
+
+/**
+ * @brief	Function that Initializes a GPIO PIN
+ *
+ * @param	Pointer to GPIO_Pin_t Struct, contains:
+ *
+ * 			- Port 			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ *
+ * 			- Pin  			(PIN0->PIN15)
+ *
+ * 			- Speed			(Macro begins with SPEED_)
+ *
+ * 			- Mode			# (Macro begins with MODE_)
+ * 							# Selects mode from Input/Output/Alternating Function/Analog
+ * 							# If Mode is Output -> Select whether Output Type is PP(Push Pull) or OD(Open Drain)
+ * 							# Select if circuit is PU(Pull Up), PD(Pull Down), or Deactivated
+ *
+ *
+ * 			- AF_CHOICE		# Selects Alternating Function from 16 choices
+ * 							# (Macro begins with AF)
+ * 							# Choices: AF0->AF_15 or AF_DEACTIVATED
+ * 							# You should enable your selected peripheral from RCC!
+ *
+ * @error	Error Status    GPIO_InvalidParameter/ GPIO_Ok
+ *
+ * @note: You should enable the GPIO Peripheral using RCC before Initialization!
+ */
+GPIO_ErrorStatus_t GPIO_InitPin(GPIO_Pin_t*PinCfg);
+
+/**
+ * @brief	Function that Sets a GPIO Pin to either HIGH/LOW
+ *
+ * @param	-Port			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ * 			-Pin			(PIN0->PIN15)
+ * 			-Value			(GPIO_LOW/GPIO_HIGH)
+ *
+ * @error	Error Status    GPIO_InvalidParameter/ GPIO_Ok
+ */
+GPIO_ErrorStatus_t GPIO_SetPinValue(void*Port, u8 Pin,u8 Value );
+
+/**
+ * @brief	Function that Gets a GPIO Pin Value
+ *
+ * @param	-Port			(GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+ * 			-Pin			(PIN0->PIN15)
+ * 			-*Value			(GPIO_LOW/GPIO_HIGH)
+ *
+ * @error	Error Status    GPIO_InvalidParameter/ GPIO_Ok
+ */
+GPIO_ErrorStatus_t GPIO_GetPinValue(void*Port, u8 Pin, u8*Value);
+#endif
