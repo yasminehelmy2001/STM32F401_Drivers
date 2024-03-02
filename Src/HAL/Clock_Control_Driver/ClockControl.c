97911a436ad8299ffc043656640b7f521dbe2cba
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2024-03-02 04:20:04 +0200

    Adding Clock Control HAL Layer

--- Src/HAL/Clock_Control_Driver/ClockControl.c
+++ Src/HAL/Clock_Control_Driver/ClockControl.c
@@ -1,3 +1,11 @@
+/*
+* File:    ClockControl.c
+* Author:  Yasmin Amr
+* Created: 2/3/2024
+* Brief:   Clock Control Driver
+* Target:  STM32F401cc
+*/
+
 #include "RCC.h"
 #include "GPIO.h"
 #include "LED.h"
@@ -5,27 +13,40 @@
 
 extern const LedCfg_t Leds[_Led_Num];
 
+/**
+ * @brief	Enables RCC Clock for all HAL Layers
+ */
 void Enable_HAL_ClockControl(void)
 {
     for(int i=0;i<_Led_Num;i++)
     {
             if(Leds[i].Port==GPIO_PORTA)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOA_RCC);
+            }
             else if(Leds[i].Port==GPIO_PORTB)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOB_RCC);
+            }
             else if(Leds[i].Port==GPIO_PORTC)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOC_RCC);
+            }
             else if(Leds[i].Port==GPIO_PORTD)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOD_RCC);
+            }
             else if(Leds[i].Port==GPIO_PORTE)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOE_RCC);
+            }
             else if(Leds[i].Port==GPIO_PORTH)
+            {
                 RCC_EnablePeriphral(AHB1,GPIOH_RCC);
+            }
             else
             {
-                
+                //Empty for MISRA
             }
-
-        
     }
 }
