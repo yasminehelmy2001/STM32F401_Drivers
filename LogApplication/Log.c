0c3be6e41bc1bd3146e8fe3a11ed17af63cd4441
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2024-03-04 01:26:40 +0200

    Loggign Task

---
+++ LogApplication/Log.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "time.h"
+
+#define NO	0
+#define YES	1
+
+#define INFO	0
+#define WARNING 1
+#define ERROR 	2
+
+/**
+ * Choose YES/NO to print output to Console
+ */
+int PRINT_TO_CONSOLE=	YES;
+
+/**
+ * Choose YES/NO to print output to File
+ */
+int PRINT_TO_FILE=		YES;
+
+/**
+ * Choose Severity Level
+ * 1) INFO: Displays info, warnings and errors
+ * 2) WARNING: Displays warnings and errors 
+ * 3) ERROR: Displays errors only
+ */
+int SEVERITY_LEVEL= 	ERROR;
+
+void printTimeFile(FILE *logFile) {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char timestamp[20];
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+    
+	if (logFile == NULL) {
+        perror("Error opening file");
+    }
+	else 
+	{
+        fprintf(logFile, "\n[%s] ", timestamp);
+	}	
+}
+
+void printTimeConsole() {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char timestamp[20];
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+	printf("\n[%s] ", timestamp);
+}
+
+
+void CheckValues(FILE*logFile,int count, ...) {
+    static int sum = 0;
+    va_list args;
+    va_start(args, count);
+    
+    // Sum up the values
+    for (int i = 0; i < count; i++) {
+        int val = va_arg(args, int);
+		    
+	if (PRINT_TO_FILE == YES) {
+            if (SEVERITY_LEVEL == INFO)
+			{
+				if (val >= 0 && val <= 100) {
+					printTimeFile(logFile);
+					fprintf(logFile, "INFO: Price %d is acceptable\n", i + 1);
+				}
+			}
+            
+            if (SEVERITY_LEVEL == INFO || SEVERITY_LEVEL == WARNING)
+			{
+				if (val > 100) {
+					printTimeFile(logFile);
+					fprintf(logFile, "WARNING: Price %d exceeds maximum price\n", i + 1);
+				}
+			}
+            if (SEVERITY_LEVEL == INFO || SEVERITY_LEVEL == WARNING || SEVERITY_LEVEL == ERROR)
+			{
+				if (val < 0) {
+					printTimeFile(logFile);
+					fprintf(logFile, "ERROR: Price %d is a negative value\n", i + 1);
+				}
+            }
+    }
+        
+    if (PRINT_TO_CONSOLE == YES) {
+            if (SEVERITY_LEVEL == INFO){
+				if (val >= 0 && val <= 100) {
+					printTimeConsole();
+					printf("INFO: Price %d is acceptable\n", i + 1);
+				}
+			}
+			if (SEVERITY_LEVEL == INFO || SEVERITY_LEVEL == WARNING)
+			{
+				if (val > 100) {
+					printTimeConsole();
+					printf("WARNING: Price %d exceeds maximum price\n", i + 1);
+				}
+			}
+			if (SEVERITY_LEVEL == INFO || SEVERITY_LEVEL == WARNING || SEVERITY_LEVEL == ERROR)
+			{            
+				if (val < 0) {
+				printTimeConsole();
+                printf("ERROR: Price %d is a negative value\n", i + 1);
+				}
+			}
+    }
+	
+}
+    va_end(args);
+
+}
+
+int main() {
+
+    FILE *logFile = fopen("time_log.txt", "a");
+	int count=3;
+    CheckValues(logFile,count,-5,200,30);
+	fprintf(logFile,"\n");
+    fclose(logFile);
+	printf("\n");
+
+    return 0;
+}
---
+++ LogApplication/README.md
@@ -0,0 +1,9 @@
+# Logging Task 
+
+### Console Output
+
+![image-20240304012508378](assets/image-20240304012508378.png)
+
+### Text File Output
+
+![image-20240304012539905](assets/image-20240304012539905.png)
