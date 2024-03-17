5af5522f661bf6853040cc1f2c6bec00c2668fcf
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2024-03-17 05:13:03 +0200

    Adding Cases

--- Src/APP/APP1_NVIC/App1.c
+++ Src/APP/APP1_NVIC/App1.c
@@ -5,6 +5,10 @@
 * Target:  STM32F401cc
 */
 
+/**
+ * Note: For Handler Debugging: Add a breakpoint inside the Handler & use Step Over.
+*/
+
 #include "APP.h"
 
 #ifdef APP  
@@ -16,11 +20,15 @@
 
 void EXTI0_IRQHandler(void)
 {
-  LED_SetStatus(LED_RED, LED_ON);
+  LED_SetStatus(LED_RED, LED_OFF);
+  LED_SetStatus(LED_YELLOW, LED_OFF);
+
 }
 
 void EXTI1_IRQHandler(void)
 {
+  LED_SetStatus(LED_RED, LED_ON);
+  NVIC_SetPending(NVIC_EXT0_INTERRRUPT);
   LED_SetStatus(LED_YELLOW, LED_ON);
 }
 
@@ -32,15 +40,24 @@ int main(void)
   NVIC_EnableInterrupt(NVIC_EXT0_INTERRRUPT);
   NVIC_EnableInterrupt(NVIC_EXT1_INTERRRUPT);
 
-  /*Testing Pending Register
-  NVIC_SetPending(NVIC_EXT0_INTERRRUPT);*/
+  /*Testing Nesting Priorities*/
+  NVIC_SetPriority(NVIC_EXT0_INTERRRUPT,0,0,SUBPRIORITY_BITS_TWO);
+  NVIC_SetPriority(NVIC_EXT1_INTERRRUPT,3,0,SUBPRIORITY_BITS_TWO);
 
-  /*Testing STIR Register
-  NVIC_GenerateSoftwareInterrupt(NVIC_EXT0_INTERRRUPT);*/
+  /** 
+   * Testing Non Nesting Priorities
+   * Problem: Returns to main after 1st ISR then goes back to 2nd ISR??
+   */
 
-  /*Nessting Nesting Priorities*/
-  NVIC_SetPriority(NVIC_EXT0_INTERRRUPT,0,0,SUBPRIRITY_BITS_TWO);
-  NVIC_SetPriority(NVIC_EXT1_INTERRRUPT,1,1,SUBPRIRITY_BITS_TWO);
+  // NVIC_SetPriority(NVIC_EXT0_INTERRRUPT,3,0,SUBPRIORITY_BITS_TWO);
+  // NVIC_SetPriority(NVIC_EXT1_INTERRRUPT,0,0,SUBPRIORITY_BITS_TWO);
+
+  /*Testing Pending Register*/
+  //NVIC_SetPending(NVIC_EXT1_INTERRRUPT);
+
+  /*Testing STIR Register*/
+  //NVIC_GenerateSoftwareInterrupt(NVIC_EXT0_INTERRRUPT);
+
 
   while(1)
   {
